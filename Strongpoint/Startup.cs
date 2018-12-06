0fd2d9546bb15570d6195de271f1efd85cf5bd36
Dhruba Dahal <dhruba.dahal@ritc.no>
2018-12-06 12:45:42 +0100

    Test github push

--- Strongpoint/Startup.cs
+++ Strongpoint/Startup.cs
@@ -35,7 +35,7 @@ namespace Strongpoint
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             //services.AddDbContext<SQLDBNORGEContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionToNorge")));
             //services.AddDbContext<SQLDBSVERIGEContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionToSverige")));
-            //hi
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
