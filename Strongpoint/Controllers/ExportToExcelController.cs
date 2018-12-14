412522fed7b6829a5cacfc2e6a4cd05f639cae19
Dhruba Dahal <dhruba.dahal@ritc.no>
2018-12-14 12:50:28 +0100

    Expert to excel implemented

---
+++ Strongpoint/Controllers/ExportToExcelController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Strongpoint.Controllers
+{
+    public class ExportToExcelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public FileResult Export(string GridHtml)
+        {
+            
+            // Get an encoding for code page 1252 (Western Europe character set).
+            Encoding cp1252 = Encoding.GetEncoding(1252);
+
+            FileResult resultFile= File(cp1252.GetBytes(GridHtml), "application/vnd.ms-excel", "Table.xls");
+            resultFile.FileDownloadName = "XmlReport.xls";
+            return resultFile;
+        }
+    }
+}
---
+++ Strongpoint/Models/ExportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Strongpoint.Models
+{
+    public class ExportViewModel
+    {
+        
+        public string Csv { get; set; }
+    }
+}
